59b73c82e27efe6c41cc409e3a3b0f0efae26417
Saichandu03 <saichanduadapa951@gmail.com>
2025-06-27 10:29:49 +0530

    User data Update

--- app.js
+++ app.js
@@ -18,10 +18,7 @@ app.use(express.static('public'));
 
 
 // DB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB connected'))
   .catch(err => console.log('MongoDB connection error:', err));
 
 
